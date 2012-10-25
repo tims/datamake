7b34e180e9303b4f3c20bb6f11b6dfffa6fe6002
Tim Sell <tim@songkick.com>
2012-10-25 16:17:51 +0100

    added license

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='datamake',
   url='https://github.com/tims/datamake',
   py_modules=['datamake'],
   scripts=['scripts/datamake'],
+  license='LICENSE.txt',
   install_requires=['urllib3==1.1','requests==0.10.0','boto','pyparseuri','simplejson']
 )
