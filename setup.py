c8e616c77d50763cd7abd1deb4a7c2c551498530
Tim Sell <tim@songkick.com>
2012-10-25 12:58:19 +0100

    use requests 0.10.0 so we can use python 2.5 (for now)

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(name='datamake',
-  version='0.0.3',
+  version='0.0.4',
   author='Tim Sell',
   author_email='trsell@gmail.com',
   url='https://github.com/tims/datamake',
   py_modules=['datamake'],
   scripts=['scripts/datamake'],
-  install_requires=['requests','boto','pyparseuri','simplejson']
+  install_requires=['requests==0.10.0','boto','pyparseuri','simplejson']
 )
