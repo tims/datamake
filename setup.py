aa2625e9e697276dc731327b40dece12d6b3f345
Tim Sell <tim@songkick.com>
2012-10-25 12:45:24 +0100

    install_requires

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='datamake',
   url='https://github.com/tims/datamake',
   py_modules=['datamake'],
   scripts=['scripts/datamake'],
-  requires=['requests','boto','pyparseuri','simplejson']
+  install_requires=['requests','boto','pyparseuri','simplejson']
 )
