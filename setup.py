762c75d7ced1cdf15fd9708d43dc4fc3509e3850
Tim Sell <tim@songkick.com>
2013-01-30 17:22:19 +0000

    incr version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='datamake',
-  version='0.0.18',
+  version='0.0.19',
   author='Tim Sell',
   author_email='trsell@gmail.com',
   url='https://github.com/tims/datamake',
