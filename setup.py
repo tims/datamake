24fdb303ef1ccafdda34360c5bade1aadd12a410
Tim Sell <tim@songkick.com>
2013-03-19 15:46:30 +0000

    version should be 0.1.0, add networkx dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(name='datamake',
-  version='0.9.0',
+  version='0.1.0',
   author='Tim Sell',
   author_email='trsell@gmail.com',
   url='https://github.com/tims/datamake',
   packages=['datamake'],
   scripts=['scripts/datamake','scripts/datamakenew'],
   license='LICENSE.txt',
-  install_requires=['urllib3','requests','boto','pyparseuri','oursql']
+  install_requires=['urllib3','requests','boto','pyparseuri','oursql','networkx']
 )
