f0fd228bef3fcedc31f7a815d744d134b57cfa4f
Tim Sell <tim@songkick.com>
2013-03-19 17:19:09 +0000

    increment version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='datamake',
-  version='0.1.0',
+  version='0.1.1',
   author='Tim Sell',
   author_email='trsell@gmail.com',
   url='https://github.com/tims/datamake',
